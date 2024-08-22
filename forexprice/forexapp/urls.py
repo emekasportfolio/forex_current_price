37d40a1fe3abc440278120ddc854493dbf1a2a05
emekasportfolio <83526269+emekasportfolio@users.noreply.github.com>
2024-08-22 09:15:09 +0000

    Deploying Django app to Heroku

--- forexprice/forexapp/urls.py
+++ forexprice/forexapp/urls.py
@@ -3,4 +3,5 @@ from .views import get_xrt_data
 
 urlpatterns = [
     path('api/xrt_data/', get_xrt_data, name='get_xrt_data'),
+    path('', get_xrt_data, name='home'),  # Root URL points to `get_xrt_data`
 ]
